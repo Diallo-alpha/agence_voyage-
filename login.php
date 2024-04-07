98b4ef7b888dbb9711c7b9bf998222265f0d832b
Haps <thiamhapstou@gmail.com>
2024-04-07 21:48:13 +0000

    la page login terminé

--- login.php
+++ login.php
@@ -1,35 +1,39 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link rel="stylesheet" href="connexion.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <!-- Intégration de Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Lien vers votre fichier CSS personnalisé -->
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-   <div class="container">
-   <div class="logo">
-        <img src="./images/building.webp" alt="logo">
+    <div class="container">
+        <form action="">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="custom-login-form">
+                        <h2 class="mb-4">Connexion</h2>
+                        <p>Nous sommes heureux de vous revoir !</p>
+                        <form action="login.php" method="POST">
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Saisir votre Email" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Saisir votre mot de passe" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block custom-btn-primary">CONNECTER</button>
+                        </form>
+                        <p class="mt-3">Pas encore inscrit ? <a href="inscription.php" class="custom-btn-secondary">Inscrivez-vous ici</a></p>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
-    
-   <div class="formulaire">
-   <h1>Connexion</h1>
-    <form action="traitement_connexion.php" method="post">
-        <input type="email" id="email" name="email" placeholder="Email" required><br>
-        <input type="password" id="motdepasse" name="motdepasse" placeholder="Mot de passe" required><br>
-        <input type="submit" value="CONNECTER">
-        <p>OU</p>
-        <div class="button">
-          <a href="inscription.php" class="button-inscription">Créer un compte</a>
 
-        </div>
-    </form>
-   
-   </div>
-   </div>
-    
+    <!-- Intégration de Bootstrap JavaScript (optionnel) -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
