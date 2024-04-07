ed11f164e0fb64d87de37f4b7473318243844e41
Haps <thiamhapstou@gmail.com>
2024-04-07 20:01:58 +0000

    le style de la page update reservation

--- updateReservation.php
+++ updateReservation.php
@@ -47,16 +47,34 @@ if(isset($_GET['id'])) {
 }
 ?>
 
-<div class="titre"><h1>Modifier ma Réservation</h1></div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier ma Réservation</title>
+    <link rel="stylesheet" href="updateReservation.css">
+     <!-- Inclure la feuille de style Bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Inclure le script Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="titre"><h1>Modifier l'état de ma Réservation</h1></div>
 <form action="" method="post">
     <input type="hidden" name="reservation_id" value="<?php echo $id_reservation; ?>">
     
-    <label for="nouvel_etat">Nouvel état de la réservation :</label>
+    <div class="form-group">
+    <label for="nouvel_etat">Nouvel état  :</label>
     <select id="nouvel_etat" name="nouvel_etat">
         <option value="confirmé" <?php if (isset($reservation_details['etat']) && $reservation_details['etat'] == "confirmé") echo "selected"; ?>>Confirmée</option>
         <option value="en attente" <?php if (isset($reservation_details['etat']) && $reservation_details['etat'] == "en attente") echo "selected"; ?>>En Attente</option>
         <option value="annulé" <?php if (isset($reservation_details['etat']) && $reservation_details['etat'] == "annulé") echo "selected"; ?>>Annulée</option>
     </select><br>
+    </div>
     
-    <input type="submit" name="submit" value="Modifier l'état">
+    <button type="submit" name="submit">Modifier l'état</button>
+
 </form>
+</body>
+</html>
