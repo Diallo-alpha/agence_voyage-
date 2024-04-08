9c5b6a4b2883645c6468c22c8cdcf41070ad0a87
Haps <thiamhapstou@gmail.com>
2024-04-08 00:24:26 +0000

    recuperation du nom et prenom dans le form de reservation

--- AddReservation.php
+++ AddReservation.php
@@ -6,9 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reserver'])) {
     $billet_id = $_POST['id'];
     $date_reservation = $_POST['date'];
     $etat = $_POST['etat'];
-    // Vous devez obtenir l'ID du client connecté
-    $client_id = 1; // Par exemple, vous devrez remplacer 1 par l'ID du client connecté
-
+    
     // Appel de la méthode pour ajouter une nouvelle réservation
     $Reservation->addReservation($billet_id, $client_id, $date_reservation, $etat);
 }
